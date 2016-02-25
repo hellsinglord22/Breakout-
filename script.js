ff6d2a523785a3bf883b8689efc448a0adc02cc2
H <heshamscorpe55@gmail.com>
2016-02-25 11:28:23 +0200

    create a canves and its 2D brush

---
+++ script.js
@@ -0,0 +1,6 @@
+/**
+take hold of the gameCanvas and create the tool that willbe used to draw
+on the canves
+ */
+var gameCanvas = document.getElementById("gameCanvas");
+var brush = gameCanvas.getContext("2d");
